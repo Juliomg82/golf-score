e6b7097c155faeb1838a5e698fb48a69fcc7615a
Julio <juliomg82@gmail.com>
2023-01-23 14:13:34 +0100

    cambiados console logs por return en index.js

--- js/index.js
+++ js/index.js
@@ -1,26 +1,32 @@
 'use strict'
 const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
-console.log(names);
 
 function golfScore(par, strokes) {
-    
-      if (strokes == 1) {
-        console.log(names[0]);
-      }else if (par - strokes >= 2) {
-        console.log(names[1]);
-      }else if (par - strokes == 1) {
-        console.log(names[2]);
-      }else if (par - strokes == 0) {
-        console.log(names[3]);
-      }else if (strokes - par == 1) {
-        console.log(names[4]);
-      }else if (strokes - par == 2) {
-        console.log(names[5]);
-      }else if (strokes - par >= 3) {
-        console.log(names[6]);
-      }
-      else
-      console.log('No se decile');  
+
+    if (strokes == 0 || par == 0) {
+        
+        return 'Los valores deben ser mayores de 0 en ambos casos';
+        
+    } else {
+        
+        if (strokes == 1) {
+            return names[0];
+        }else if (par - strokes >= 2) {
+            return names[1];
+        }else if (par - strokes == 1) {
+            return names[2];
+        }else if (par - strokes == 0) {
+            return names[3];
+        }else if (strokes - par == 1) {
+            return names[4];
+        }else if (strokes - par == 2) {
+            return names[5];
+        }else if (strokes - par >= 3) {
+            return names[6];
+        }
+    }
+
+  return 'Ejecutada la función golfScore';               
 }
   
-golfScore(2, 7);
+golfScore(5, 1);
