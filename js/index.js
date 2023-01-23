f61b90e465d3c702a590271fd0464d3dd6fee06b
Julio <juliomg82@gmail.com>
2023-01-23 13:30:15 +0100

    primera version funcionando index.js

--- js/index.js
+++ js/index.js
@@ -1,25 +1,26 @@
+'use strict'
 const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+console.log(names);
+
 function golfScore(par, strokes) {
-    // Only change code below this line
-      
+    
       if (strokes == 1) {
         console.log(names[0]);
+      }else if (par - strokes >= 2) {
+        console.log(names[1]);
+      }else if (par - strokes == 1) {
+        console.log(names[2]);
+      }else if (par - strokes == 0) {
+        console.log(names[3]);
+      }else if (strokes - par == 1) {
+        console.log(names[4]);
+      }else if (strokes - par == 2) {
+        console.log(names[5]);
+      }else if (strokes - par >= 3) {
+        console.log(names[6]);
       }
-      else if (strokes == par - 2) {
-        console.log (names[1]);
-      }else if (strokes == par - 1) {
-        console.log (names[2]);
-      }else if (strokes == par) {
-        console.log (names[3]);
-      }else if (strokes == par + 1) {
-        console.log (names[4]);
-      }else if (strokes == par + 2) {
-        console.log (names[5]);
-      }else if (strokes == par + 3) {
-        console.log (names[6]);
-      }
-        console.log ('The number of strokes should be greater than 0');
-    // Only change code above this line
-  }
+      else
+      console.log('No se decile');  
+}
   
-  golfScore(4, 9);
+golfScore(2, 7);
